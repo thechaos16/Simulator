6268fde1391882f278705522d159b458f5ef5689
unknown <>
2016-03-05 18:41:02 +0900

    ai player changed

--- indian_poker/artificial_intelligence/auto_player.py
+++ indian_poker/artificial_intelligence/auto_player.py
@@ -5,16 +5,20 @@ Created on Fri Feb 19 23:30:59 2016
 @author: thech
 """
 
+def decision_maker(cur_deck,opponent_card):
+    pass
 
 # conditional probability (p(a|d), a: my card, d: deck)
-def my_card_pdf(deck):
-    card_pdf = []
+# output is list of pmf
+def my_card_pmf(deck):
+    card_pmf = []
     remain = deck.deck_result()
     for i in range(10):
-        card_pdf.append(round(float(remain.count(i+1))/float(len(remain)),3))
-    return card_pdf
+        card_pmf.append(round(float(remain.count(i+1))/float(len(remain)),3))
+    return card_pmf
 
-# winning probability (d: deck in table, c: opponent's card)
+# winning probability
+# win/(entire-tie)
 def winning_probability(deck,opponent_card):
     remain = deck.deck_result()
     if remain==0:
@@ -30,25 +34,18 @@ def winning_probability(deck,opponent_card):
 
 # predict my card (need to be improved)
 def my_card_prediction(deck,opponent_move):
-    card_pdf = []
-    prior = my_card_pdf(deck)
+    card_pmf = []
+    prior = my_card_pmf(deck)
     for i in range(10):
         # opponent's winning probability based on my cards (unknown)
         temp = winning_probability(deck,i+1)
         # opponent folds
         if opponent_move==0:
-            card_pdf.append(round(2*prior[i]*(1-temp),3))
+            card_pmf.append(round(2*prior[i]*(1-temp),3))
         # opponent bets
         else:
-            card_pdf.append(round(2*prior[i]*temp,3))
-    return card_pdf
-
-# function based budget (player, opponent, move of opponent) 
-def budget(p,o,m):
-    bp = p.getmoney()
-    bo = o.getmoney()
-    fun = bp*bo*m
-    return 0
+            card_pmf.append(round(2*prior[i]*temp,3))
+    return card_pmf
 
 # select my move (d: deck, m: opponent's move, c: opponent's card)
 def mmove(d,m,c):
@@ -60,3 +57,7 @@ def mmove(d,m,c):
         elif i+1-c>0:
             res+=l[i]
     return res
+    
+
+if __name__=='__main__':
+    pass
